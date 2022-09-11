e803ad8e6476f0bc40a9bfbc99c81c3a05abe34c
Roro1839 <you@example.com>
2022-09-10 22:06:00 -0700

    ass

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 /**
+* main-Entry point
 *
-* * main-Entry point
-*
-* *
-*
-* * Return: 0 if successful
-*
-* */
+* Return: 0 if successful
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
