522938e1f6d8902d63b0a4a561cf299b37753243
Roro1839 <you@example.com>
2022-09-15 15:26:52 -0700

    ass

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
-* main-Entry point
-*
-* Return: 0 if successful
-*/
-int _putchar(char c)
+ * main - prints _putchar
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
 {
-return (write(1, &c, 1));
+	char word[9] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+	return (0);
 }
