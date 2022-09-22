3fbf1fc87a4be762871c7b0fc038ddad9977624c
Roro1839 <you@example.com>
2022-09-22 13:22:06 -0700

    ass

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
-
 /**
- * get_len - returns the length of a string
- *
- * @str: char pointer
- *
- * Return: int
+ *_strncat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *@n: the number of bytes to use from src.
+ *Return: string.
  */
 
-int get_len(char *str)
+char *_strncat(char *dest, char *src, int n)
 {
-	int len = 0;
+int i = 0, k = 0;
 
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
+while (dest[i] != '\0')
+i++;
 
-	return (len);
+while (src[k] != '\0' && n > k)
+{
+dest[i] = src[k];
+k++;
+i++;
+}
+if (n > 0)
+{
+dest[i] = '\0';
+}
+
+return (dest);
 }
